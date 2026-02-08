7fa9607a16afd4f8fd367cf9ff363953b8ecae59
vishnu-vikas-5 <dvp.vishnuvikas@gmail.com>
2026-02-08 11:02:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+emailjs.init('ocgOtEmP6FaVreNbU');
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        btn.value = 'Sending...';
+
+        const serviceID = 'service_n6redyo';
+        const templateID = 'template_y7mv4p9';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+    });
